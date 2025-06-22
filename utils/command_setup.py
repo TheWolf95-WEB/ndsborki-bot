e287b0d9bb235a0099e07bf01cb50394a39bcdb8
ND_Web <55359442+TheWolf95-WEB@users.noreply.github.com>
2025-06-23 01:05:03 +0300

    Update command_setup.py

--- utils/command_setup.py
+++ utils/command_setup.py
@@ -1,4 +1,5 @@
-from telegram import BotCommand
+from telegram import BotCommand, BotCommandScopeDefault, BotCommandScopeChat
+import os
 
 public_commands = [
     BotCommand("help", "📩 Помощь и поддержка"),
@@ -9,20 +10,21 @@ public_commands = [
 
 admin_commands = [
     BotCommand("restart", "🔁 Перезапустить бота"),
+    BotCommand("update", "♻ Обновить и сбросить"),
     BotCommand("log", "🪵 Последние 30 строк логов"),
     BotCommand("status", "📊 Статистика и состояние"),
     BotCommand("check_files", "🗂 Проверка модулей"),
     BotCommand("delete", "❌ Удалить сборку"),
     BotCommand("stop_delete", "⛔ Остановить удаление"),
-    *public_commands
+    *public_commands,
 ]
 
 async def set_commands(app):
-    await app.bot.set_my_commands(public_commands)
-    
+    await app.bot.set_my_commands(public_commands, scope=BotCommandScopeDefault())
+
     for admin_id in os.getenv("ALLOWED_USERS", "").split(","):
-        if admin_id.isdigit():
+        if admin_id.strip().isdigit():
             await app.bot.set_my_commands(
                 admin_commands,
-                scope={"type": "chat", "chat_id": int(admin_id)}
+                scope=BotCommandScopeChat(chat_id=int(admin_id))
             )
