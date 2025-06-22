3daff04fde39a8d570d47136d84dbda39be48b6a
ND_Web <55359442+TheWolf95-WEB@users.noreply.github.com>
2025-06-22 19:33:40 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,6 +12,7 @@ from conversations.delete import delete_conv, stop_delete_callback
 
 from utils.logging_config import configure_logging
 from utils.restart_notifier import notify_restart
+from utils.command_setup import set_commands
 
 load_dotenv()
 configure_logging()
@@ -21,6 +22,7 @@ TOKEN = os.getenv("BOT_TOKEN")
 # ⏳ Комбинируем запуск: уведомление при рестарте + главное меню
 async def full_startup(app):
     await notify_restart(app)
+    await set_commands(app)
     await home_cmd.on_startup(app)
 
 # 🔁 Создаём приложение
