e7c4249f18a353d7970cc00fda448a31df882a40
ND_Web <55359442+TheWolf95-WEB@users.noreply.github.com>
2025-06-26 15:25:25 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,27 +3,30 @@ import logging
 import os
 from dotenv import load_dotenv
 from telegram.ext import ApplicationBuilder
-from utils.logging_config import configure_logging
-from utils.command_setup import set_commands, clear_all_scopes
-from utils.keyboards import get_main_menu
-from handlers.admin import admin_handlers
+
 from handlers.start import start_handler
 from handlers.help import help_handler
 from handlers.home import home_cmd, home_button
 from handlers.test import test_handler
+from handlers.admin import admin_handlers
 from conversations.view import view_conv
 from conversations.add import add_conv
 from conversations.delete import delete_conv, stop_delete_callback
+from utils.logging_config import configure_logging
+from utils.command_setup import set_commands, clear_all_scopes
+from utils.keyboards import get_main_menu
 
 load_dotenv(dotenv_path=".env")
 configure_logging()
 TOKEN = os.getenv("BOT_TOKEN")
 
 async def on_startup(app):
+    logging.info("Устанавливаю команды…")
     await clear_all_scopes(app)
     await set_commands(app)
     await asyncio.sleep(1)
 
+    # Если был рестарт — уведомляем пользователя
     flag = "restart_message.txt"
     if os.path.exists(flag):
         with open(flag, encoding="utf-8") as f:
@@ -40,7 +43,6 @@ async def on_startup(app):
         finally:
             os.remove(flag)
 
-
 app = (
     ApplicationBuilder()
     .token(TOKEN)
@@ -48,7 +50,7 @@ app = (
     .build()
 )
 
-# Регистрация хэндлеров
+# Регистрируем хэндлеры
 app.add_handler(start_handler)
 app.add_handler(help_handler)
 app.add_handler(home_cmd)
