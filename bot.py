a23279837539b96f3e0d210a7d33347446951991
ND_Web <55359442+TheWolf95-WEB@users.noreply.github.com>
2025-06-26 15:18:17 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,21 +2,18 @@ import asyncio
 import logging
 import os
 from dotenv import load_dotenv
-
 from telegram.ext import ApplicationBuilder
-from telegram import ReplyKeyboardMarkup
-
+from utils.logging_config import configure_logging
+from utils.command_setup import set_commands, clear_all_scopes
+from utils.keyboards import get_main_menu
+from handlers.admin import admin_handlers
 from handlers.start import start_handler
 from handlers.help import help_handler
 from handlers.home import home_cmd, home_button
-from handlers.admin import admin_handlers
 from handlers.test import test_handler
 from conversations.view import view_conv
 from conversations.add import add_conv
 from conversations.delete import delete_conv, stop_delete_callback
-from utils.logging_config import configure_logging
-from utils.command_setup import set_commands, clear_all_scopes
-from utils.keyboards import get_main_menu
 
 load_dotenv(dotenv_path=".env")
 configure_logging()
@@ -45,9 +42,6 @@ async def on_startup(app):
         finally:
             os.remove(flag)
 
-
-
-
 app = (
     ApplicationBuilder()
     .token(TOKEN)
@@ -60,15 +54,13 @@ app.add_handler(start_handler)
 app.add_handler(help_handler)
 app.add_handler(home_cmd)
 app.add_handler(home_button)
-
 for h in admin_handlers:
     app.add_handler(h)
-
 app.add_handler(view_conv)
 app.add_handler(add_conv)
 app.add_handler(delete_conv)
 app.add_handler(stop_delete_callback)
 app.add_handler(test_handler)
 
-print("Бот запущен…")
+logging.info("Бот запущен…")
 app.run_polling()
